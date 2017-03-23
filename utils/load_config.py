854c9b867d21514b43dcd97e1b223a75435c3d81
lanshiyi <lanshiyi@megvii.com>
2017-03-23 18:25:16 +0800

    fix load config

--- utils/load_config.py
+++ utils/load_config.py
@@ -1,6 +1,18 @@
 import config
 
 import json
+from easydict import EasyDict as Edict
+
+
+def assign_json(obj, key, value):
+    if obj.get(key, None) is not None:
+        if isinstance(value, dict) and (isinstance(obj[key], Edict) or isinstance(obj[key]. dict)):
+            for k,v in value.iteritems():
+                assign_json(obj[key], k, v)
+        else:
+            obj[key] = value
+    else:
+        obj[key] = value
 
 
 def load_config(config_path='examples/config/coco.json'):
@@ -10,5 +22,5 @@ def load_config(config_path='examples/config/coco.json'):
     with open(config_path, "r") as f:
         obj = json.load(f)
     for k,v in obj.iteritems():
-        config.__dict__[k] = v
+        assign_json(config.__dict__, k, v)
 
